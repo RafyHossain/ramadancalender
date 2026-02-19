799cc2cfecd2403f95f476cb73574c8526be2df5
RafyHossain <rafyhossain56@gmail.com>
2026-02-19 16:28:19 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { Analytics } from '@vercel/analytics/react';
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) => console.log("SW Error:", err));
+  });
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
