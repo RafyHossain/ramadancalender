a49397eb86143a0651fbf73733cd1a7c2e3bf57e
RafyHossain <rafyhossain56@gmail.com>
2026-02-19 16:35:50 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { Analytics } from '@vercel/analytics/react';
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() => console.log("Service Worker Registered"))
-      .catch((err) => console.log("SW Error:", err));
-  });
-}
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
+
 
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Ramadan 2026 - Sehri & Iftar Time',
+        short_name: 'Ramadan 2026',
+        description: 'Bangladesh Sehri & Iftar Time Schedule',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/ramadanlogo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/ramadanlogo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
